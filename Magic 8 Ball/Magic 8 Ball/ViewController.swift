4ff6576050636f7c7446d47fb160fe25acaa0e54
Dariusz Czochara <darek@Mac-mini-Dariusz.local>
2018-09-25 21:11:15 +0200

    New Xcode Project

---
+++ Magic 8 Ball/Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Dariusz Czochara on 25/09/2018.
+//  Copyright © 2018 Dariusz Czochara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
