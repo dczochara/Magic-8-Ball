32bba845edfee56f3b6b5caeff1198d45ea4cb82
Dariusz Czochara <darek@Mac-mini-Dariusz.local>
2018-09-28 20:12:11 +0200

    Main.Storyboad nad new method with arc4random.

--- Magic 8 Ball/Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/Magic 8 Ball/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
 
+    var randomBallNumber = 0
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        newBallImage()
+    }
+    @IBAction func askButtonPressed(_ sender: Any) {
+        
+        newBallImage()
+        
+    }
+    
+    func newBallImage(){
+        randomBallNumber = Int(arc4random_uniform(5))
+        
+        imageView.image = UIImage(named: "ball\(randomBallNumber)")
+    }
+    
 }
 
